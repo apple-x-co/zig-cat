8e7194c7aea290cc4ce27af71aba8148f8060a4b
apple-x-co <apple.x.co@gmail.com>
2022-08-05 10:04:08 +0900

    parse arg

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,23 @@ pub fn main() anyerror!void {
     const args = try std.process.argsAlloc(pageAllocator);
     defer pageAllocator.free(args);
 
-    std.debug.print("number of args: {}\n", .{args.len});
-    for (args) |arg| {
+    const stdout = std.io.getStdOut().writer();
+
+    if (args.len == 1) {
+        try stdout.print("Missing filename\n", .{});
+        return;
+    }
+
+    // std.debug.print("number of args: {}\n", .{args.len});
+    for (args) |arg, index| {
+        if (index == 0) {
+            continue;
+        }
+
         std.debug.print("{s}\n", .{arg});
+
+        // var file = try std.fs.cwd().openFile(arg);
+        // defer file.close();
+        // std.debug.print("{s}\n", .{file.stat});
     }
 }
